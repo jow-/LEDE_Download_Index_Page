a82da54f93d5dfebffe7bba85e8fd457cff25c93
Rich Brown <richb.hanover@gmail.com>
2020-06-12 22:31:25 -0400

    Apply new OpenWrt style
    This incorporates suggested changes by Rich with minor CSS improvements:
    * Use final logo as png version (svg may allow XSS)
    * Increase logo size
    * Minimal width for checksum, size and time
    * Update screenshots
    * Reformat README
    * Update sample data
    * Move test files to subfolder (test/)
    * Replace KeyCDN with fastly.com in footer
    
    Signed-off-by: Paul Spooren <mail@aparcar.org>

---
+++ samples/sha256sums.sig
@@ -0,0 +1,2 @@
+untrusted comment: verify with /master/signall.5870/usign.pub
+RWT5S53W/rrJY+//uhW2hCfn/UEaHzIJ5KYjjSVyZ4A1mHgIZXzhGHC905FHNE16Y7ZNARLk2f+ZfIshZqZMxg3L+PRoXF/iOwo=
--- test.sh
+++ test.sh
@@ -3,11 +3,42 @@
 # Test script that sets environmental variables, then launches the dir-index.cgi script
 # Original script at: https://gist.github.com/jow-/743363c332d09cb58a60dd1f216b6ee4
 
+# Script writes the two files into the test/ directory
+#
+# 1) targets.html shows the "targets" directory
+# 2) directory.html displays a standard (non "target") directory
+
+# Open the pages in a browser and reload after each run
+
 # To run:
 #   cd <main directory of repo>
-#   test.sh > SampleData/index.html
+#   sh test.sh
 
-export DOCUMENT_ROOT="./SampleData/"                          # becomes $phys
-export PATH_INFO="releases/17.01.0/targets/ar71xx/generic/"   # becomes $virt
+# Display a "target" directory with OpenWrt firmware binary files
+#
+export DOCUMENT_ROOT="./samples/"                   # becomes $phys
+export PATH_INFO="releases/19.07.6/targets/x86/64/" # becomes $virt
 
-perl dir-index.cgi
+perl dir-index.cgi >./test/targets.html
+
+# Display a simple directory listing
+#
+export DOCUMENT_ROOT="./" # becomes $phys
+export PATH_INFO="./"     # becomes $virt
+
+perl dir-index.cgi >./test/directory.html
+
+# Display 404 page
+#
+export DOCUMENT_ROOT="./glorph"                             # becomes $phys
+export PATH_INFO="releases/17.01.0/targets/ar71xx/generic/" # becomes $virt
+
+perl dir-index.cgi >./test/404.html
+
+# Display JSON output
+#
+export DOCUMENT_ROOT="./samples"             # becomes $phys
+export PATH_INFO="releases/19.07.6/targets/" # becomes $virt
+export QUERY_STRING="json"
+
+perl dir-index.cgi >./test/output.json
